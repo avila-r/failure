1d90a878856ace39739753051fb9a0ed315c6097
r. ávila <avila.dev@outlook.com>
2025-01-20 20:48:00 -0300

    feat: add trait.New()

--- trait/trait.go
+++ trait/trait.go
@@ -1,4 +1,15 @@
 package trait
 
+import "github.com/avila-r/failure/id"
+
 type Trait struct {
+	ID    uint64
+	Label string
+}
+
+func New(label string) Trait {
+	return Trait{
+		ID:    id.Next(),
+		Label: label,
+	}
 }
